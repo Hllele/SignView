9537f0a4518591215214b09321a5688e3aba55bb
Hllele <958037237@qq.com>
2017-09-11 09:17:09 +0800

    添加资源文件

---
+++ app/src/main/java/com/llele/sign/MyApplication.java
@@ -0,0 +1,21 @@
+package com.llele.sign;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Author： huanglele on 2017/9/11.
+ */
+
+public class MyApplication extends Application {
+    private static Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext(){
+        return context;
+    }
+}
